b3f86435e8e1d483618dca521c0ae336caff65a7
ivgomez <iv.gomez@hotmail.com>
2020-10-06 20:57:35 -0500

    adding docs

---
+++ docs/precache-manifest.5037470383c8c2ae23a2a90dc566d274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ad520bf618c4d98fa929ac0ce3b950f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac97f8c53e76b76d88f2",
+    "url": "/static/css/main.33aa2db3.chunk.css"
+  },
+  {
+    "revision": "ccd9761b7db298367323",
+    "url": "/static/js/2.dab6626b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dab6626b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac97f8c53e76b76d88f2",
+    "url": "/static/js/main.760fc5d5.chunk.js"
+  },
+  {
+    "revision": "0a62db85d9147c438712",
+    "url": "/static/js/runtime-main.2bd8def6.js"
+  }
+]);
